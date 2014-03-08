8763aee36fcdd00d490e62ad690be6adfc965e47
Dhruv Grover <dhgrover@ucsd.edu>
2014-03-08 14:51:16 -0800

    Revert dd63e61..a173459
    This rolls back to commit dd63e6145b167d3336733d46c4b4130b2c09de9b.

--- fmfRecord/stdafx.h
+++ fmfRecord/stdafx.h
@@ -3,23 +3,13 @@
 // are changed infrequently
 //
 
-#if defined(WIN32) || defined(WIN64)
-
 #pragma once
 
-#ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.                   
-#define _WIN32_WINNT 0x0600	// Change this to the appropriate value to target other versions of Windows.
-#endif			
-
-#define _CRT_SECURE_NO_WARNINGS				
+#include "targetver.h"
 
 #include <windows.h>
 #include <tchar.h>
 
-#elif defined(MACOSX)
 
-#else
 
-#include <stdio.h>
-
-#endif
+// TODO: reference additional headers your program requires here
