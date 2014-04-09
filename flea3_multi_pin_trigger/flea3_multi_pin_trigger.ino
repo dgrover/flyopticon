f1a567b9c9054b0a3a2039fcfb78a537ce8b41a3
Dhruv Grover <dhgrover@ucsd.edu>
2014-04-09 15:47:19 -0700

    Working version of multi-pin ttl pulse

--- flea3_multi_pin_trigger/flea3_multi_pin_trigger.ino
+++ flea3_multi_pin_trigger/flea3_multi_pin_trigger.ino
@@ -1,12 +1,17 @@
+// TTL pulse to Arduino Mega 2560 pins 0,2,3
+// Unlike the Uno, the Mega ports map differently. 
+// Arduino pins 0,2,3 map to PORTE 0,4,5 
+
+//set Port E pins 0-7 as OUTPUT
 void setup()
 {
-  DDRB = B11111111;
+  DDRE = B11111111;
 }
 
 void loop()
 {
-  PORTB = B11100000;
+  PORTE = B00110001; //Set pins 0,4,5 as HIGH
   delayMicroseconds(3333);
-  PORTB = B00000000;
+  PORTE = B00000000; //Set pins 0,4,5 as LOW
   delayMicroseconds(3333);
 }
