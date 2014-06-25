31069bb7d762780f6ecfbd652a42427f34413f7d
Dhruv Grover <dhgrover@ucsd.edu>
2014-06-25 12:10:19 -0700

    project structure recreated. No code changes, working version.

---
+++ fmfrecord/stdafx.h
@@ -0,0 +1,23 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <tchar.h>
+
+#include <FlyCapture2.h>
+#include <omp.h>
+#include <queue>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+// TODO: reference additional headers your program requires here
